5e0a03daf294779de8710e2da4c73d851d832657
Hsinheng Hsu <hsin@Hsinhengs-MBP.fios-router.home>
2018-08-25 13:20:52 -0400

    express

--- node_js/server.js
+++ node_js/server.js
@@ -1,17 +1,49 @@
-const http = require('http');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const server = http.createServer((request, response) => {
-	//response.setHeader('Content-Type', 'text/html');
-	response.setHeader('Content-Type', 'application/json');
-	const user = {
-		name:'Hsin',
-		age:38
-	}
-	//response.end('<h1>Hellloooooooooooo</h1>')
-	response.end(JSON.stringify(user))
-	// console.log('Headers', request.headers)
-	console.log('method', request.method)
-	console.log('url', request.url)
+const app = express();
+
+// this is the middleware that allows you to work with the data before passing to the routes
+/*
+app.use((req, res, next) => {
+	console.log('i am the middleware')
+	next()
+})
+*/
+
+app.use(bodyParser.urlencoded({extened: false}))
+app.use(bodyParser.json())
+
+
+// this is for only GET request. 
+app.get('/profile', (req, res) => {
+	const user = {name:"Hsin", age:30}
+	res.send(user)
 })
 
-server.listen(3000); //Port number
+app.get('/', (req, res) => {
+	res.send("<h1>helloooooo</h1>")
+})
+
+app.get('/test', (req, res) => {
+	res.send('this is a test')
+})
+
+// for post request (you can use postman to send post request)
+app.post('/post',(req, res) => {
+	console.log(req.body)
+	res.send("Post request successful!")
+})
+
+app.get('/query', (req, res) => {
+	//get the query e.g., 'localhost:3000/query?name=Hsin&age=50'
+	console.log(req.query)
+    console.log(req.body)
+    console.log(req.header)
+    console.log(req.params)
+
+	res.send("sending query")
+})
+
+app.listen(3000)
+
